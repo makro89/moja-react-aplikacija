d50f7219454d3ea1f0af747be55f5b3e00f1f080
makro89 <mgalic99@gmail.com>
2023-05-15 18:15:39 +0200

    promjene

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; 
-import AppAPI from './components/AppAPI';
 //import App from './components/App';
 // ovdje radimo switch izmedju singlepage app, konkretno za zadatak Liste ucitavamo App iz AppLista
 // 5.13 - Liste
@@ -10,7 +9,16 @@ import AppAPI from './components/AppAPI';
 //import App from './components/AppForme1';
 // 5.15 - Forme 2
 //import AppForme2 from './components/AppForme2';
- 
+// 5.16 - Context
+//import AppContext from './components/AppContext';
+// 5.17 - HOC
+//import AppHOC from './components/AppHOC';
+// 5.18 - ReactRouter
+// import AppRouter from './components/AppRouter';
+// 5.19 - API
+//import AppAPI from './components/AppAPI';
+// 5.20 - GraphQL
+import AppGraphQL from './components/AppGraphQL';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppAPI/>);
+root.render(<AppGraphQL/>);
